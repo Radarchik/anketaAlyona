0dfebe0eb9579fa1efc09965818a1d0961a6bf53
synovych_upcdt <tarasz.szinyovics@upc.ua>
2022-06-08 20:01:12 +0300

    change for Mariya

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,7 +10,7 @@ import {QuestionsService} from './questions.service';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit, OnDestroy {
-  public title = 'anketaVika';
+  public title = 'anketaMariya';
   startForm: FormGroup;
   questions: Question[];
   rightPassword = '777333';
